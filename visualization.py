1b207e34268079d132c1a44e901364093e2fb790
Rotem Agmon <rotem_agmon@cs.huji.ac.il>
2023-09-14 11:26:56 +0200

    Add files via upload

---
+++ visualization.py
@@ -0,0 +1,11 @@
+
+from plots import *
+from pre_process import *
+
+# for test in computer do not have connection to the data add test=True in create_df parameters
+df = create_df()
+combine_all_daily_plots(df)
+
+
+
+
